68f4b676e933a22a288f4eb4c11d8d33eb52e7c4
Ivan-ea-69 <ivan.ea.1231@gmail.com>
2025-07-22 20:59:26 +0800

    MakeCode 專案的初始檔案

---
+++ images.g.ts
@@ -0,0 +1,101 @@
+// 自動產生的程式碼。請勿編輯。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image3":
+            case "DDDD0":return img`
+. . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+. 5 f f f f 5 5 5 5 f f f f 5 . 
+5 5 f f f f 5 5 5 5 f f f f 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 1 f f 1 5 5 5 5 1 f f 1 5 5 
+5 5 1 f f 1 5 5 5 5 1 f f 1 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 f 5 5 5 5 5 5 5 5 5 5 f 5 5 
+5 5 5 f 5 5 5 5 5 5 5 5 f 5 5 5 
+5 5 5 5 f f f f f f f f 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 . . . . 5 5 5 5 5 5 
+5 5 5 5 5 5 . . . . 5 5 5 5 5 5 
+. 5 5 5 5 . . . . . . 5 5 5 5 . 
+`;
+            case "image4":
+            case "Y":return img`
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. 8 f f f f 8 8 8 8 f f f f 8 . 
+8 8 f f f f 8 8 8 8 f f f f 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 1 f f 1 8 8 8 8 1 f f 1 8 8 
+8 8 1 f f 1 8 8 8 8 1 f f 1 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 f 8 8 8 8 8 8 8 8 8 8 f 8 8 
+8 8 8 f 8 8 8 8 8 8 8 8 f 8 8 8 
+8 8 8 8 f f f f f f f f 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 . . . . 8 8 8 8 8 8 
+8 8 8 8 8 8 . . . . 8 8 8 8 8 8 
+. 8 8 8 8 . . . . . . 8 8 8 8 . 
+`;
+            case "image1":
+            case "DDDD":return img`
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. 7 f f f f 7 7 7 7 f f f f 7 . 
+7 7 f f f f 7 7 7 7 f f f f 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 1 f f 1 7 7 7 7 1 f f 1 7 7 
+7 7 1 f f 1 7 7 7 7 1 f f 1 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 f 7 7 7 7 7 7 7 7 7 7 f 7 7 
+7 7 7 f 7 7 7 7 7 7 7 7 f 7 7 7 
+7 7 7 7 f f f f f f f f 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 . . . 7 7 7 7 7 7 7 
+7 7 7 7 7 7 . . . . 7 7 7 7 7 7 
+. 7 7 7 7 . . . . . . 7 7 7 7 . 
+`;
+            case "image2":
+            case "Y2":return img`
+. . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+. 2 f f f f 2 2 2 2 f f f f 2 . 
+2 2 f f f f 2 2 2 2 f f f f 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 1 f f 1 2 2 2 2 1 f f 1 2 2 
+2 2 1 f f 1 2 2 2 2 1 f f 1 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 f 2 2 2 2 2 2 2 2 2 2 f 2 2 
+2 2 2 f 2 2 2 2 2 2 2 2 f 2 2 2 
+2 2 2 2 f f f f f f f f 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 . . . . 2 2 2 2 2 2 
+2 2 2 2 2 2 . . . . 2 2 2 2 2 2 
+. 2 2 2 2 . . . . . . 2 2 2 2 . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動產生的程式碼。請勿編輯。
